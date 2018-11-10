96fc8246fa7428da5a8c520ffcaa03cf40ab7547
Eric <ewomer@hotmail.com>
2018-11-10 14:53:00 -0500

    ran clang-format.sh on the project tree

--- src/skeleton/skeleton.h
+++ src/skeleton/skeleton.h
@@ -21,20 +21,20 @@ public:  /// somehing ///
     // Public Interface
     Skeleton(){};
     virtual ~Skeleton(){};
-    virtual int main() = 0;
-    virtual int main(int argv, char* argc[]) = 0;
-    virtual int main(std::vector<std::string>& params) = 0;
-    virtual int size() = 0;
+    virtual int               main() = 0;
+    virtual int               main(int argv, char* argc[]) = 0;
+    virtual int               main(std::vector<std::string>& params) = 0;
+    virtual int               size() = 0;
     virtual const std::string name() = 0;
-    virtual void help() = 0;
-    virtual std::string name() const = 0;
-    virtual void name(const std::string& name) = 0;
-    virtual Version_t version() const = 0;
-    virtual void version(const Version_t& version) = 0;
+    virtual void              help() = 0;
+    virtual std::string       name() const = 0;
+    virtual void              name(const std::string& name) = 0;
+    virtual Version_t         version() const = 0;
+    virtual void              version(const Version_t& version) = 0;
 
 protected:
     // Private data members
-    std::string app_name;
+    std::string              app_name;
     std::vector<std::string> app_description;
 
     Version_t version_number;
