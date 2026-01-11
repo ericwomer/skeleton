d0c5f99a3134df3e0b3f07415610e75af0080426
ewomer <ewomer@hotmail.com>
2026-01-10 21:16:32 -0500

    ApplicationInterface: Reformat file

--- src/Interface/ApplicationInterface.h
+++ src/Interface/ApplicationInterface.h
@@ -10,7 +10,6 @@
 
 namespace ew {
 
-
 class ApplicationInterface : public Log {
 public:
   // Data Types
@@ -32,8 +31,8 @@ public:
 
   // Public Interface
   // Constructor && Destructor
-  ApplicationInterface()          = default;
-  virtual ~ApplicationInterface() = default;
+  ApplicationInterface()                          = default;
+  virtual ~ApplicationInterface()                 = default;
 
   // Methods
   /**
@@ -42,24 +41,24 @@ public:
    * @param argv
    * @return
    */
-  virtual auto main(int argc, char** argv) -> int = 0;
+  virtual auto main(int argc, char **argv) -> int = 0;
   virtual auto size() -> int                      = 0;
   virtual auto help() const -> void               = 0;
-  virtual auto init(int argc, char** argv) -> int = 0;
+  virtual auto init(int argc, char **argv) -> int = 0;
 
-  virtual auto info() const -> const Info& { return m_info; };
+  virtual auto info() const -> const Info & { return m_info; };
   virtual auto info(Info info) -> void { m_info = info; };
 
-  virtual auto flags() -> std::vector<std::string>& { return m_flags; };
+  virtual auto flags() -> std::vector<std::string> & { return m_flags; };
   virtual auto flags(std::vector<std::string> flags) -> void { m_flags = flags; }
 
-  virtual auto name() const -> const std::string& { return m_info.app_name; };
-  virtual auto name(const std::string& name) -> void { m_info.app_name = name; };
+  virtual auto name() const -> const std::string & { return m_info.app_name; };
+  virtual auto name(const std::string &name) -> void { m_info.app_name = name; };
 
-  virtual auto description() const -> const std::vector<std::string>& { return m_info.app_description; };
+  virtual auto description() const -> const std::vector<std::string> & { return m_info.app_description; };
   virtual auto description(std::vector<std::string> desc) -> void { m_info.app_description = desc; };
 
-  virtual auto version() const -> const Version& { return m_info.version_number; };
+  virtual auto version() const -> const Version & { return m_info.version_number; };
   virtual auto version(Version version) -> void { m_info.version_number = version; };
 
 private:
@@ -75,16 +74,16 @@ private:
  * @param obj the object being written to the output stream
  * @return std::ostream&
  */
-inline auto operator<<(std::ostream& out, const ApplicationInterface::Version& obj) -> std::ostream&
+inline auto operator<<(std::ostream &out, const ApplicationInterface::Version &obj) -> std::ostream &
 {
   return out << obj.major << "." << obj.minor << "." << obj.patch << "." << obj.compile;
 }
 
 template <typename T>
-inline auto operator<<(std::ostream& out, const std::vector<T>& obj) -> std::ostream&
+inline auto operator<<(std::ostream &out, const std::vector<T> &obj) -> std::ostream &
 {
   out << "[";
-  for (auto i = 0; i < obj.size() - 1; ++i) {
+  for(auto i = 0; i < obj.size() - 1; ++i) {
     out << obj[i] << ", ";
   }
   out << obj[obj.size() - 1] << "]";
